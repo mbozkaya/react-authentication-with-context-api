a361a0e83867170d1f5caf35e7d43f3d75857641
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-10-17 23:15:57 +0300

    login page

--- ui/src/views/Login.js
+++ ui/src/views/Login.js
@@ -1,40 +1,112 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navigate } from 'react-router';
 import { AuthContext } from '../contexts/AuthContext';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import {
+    Box,
+    Button,
+    Container,
+    TextField,
+    makeStyles
+} from '@material-ui/core';
+import Page from '../components/page';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: theme.palette.background.dark,
+        height: '100%',
+        paddingBottom: theme.spacing(3),
+        paddingTop: theme.spacing(3)
+    }
+}));
+
+
 
 const Login = () => {
-    const [loginState, setLoginState] = useState({
-        email: '',
-        password: '',
-    })
+    const classes = useStyles();
     return (
         <AuthContext.Consumer>
             {({ authorize, onLogin }) => (
                 (authorize ? <Navigate to='/dashboard' /> :
-                    <div>
-                        <p>Login Page</p>
-                        <form onSubmit={e => {
-                            e.preventDefault();
-                            if (true) {//Validation
-                                const { ...model } = loginState;
-                                onLogin(model);
-                                console.log(model);
-                            }
-                            return false;
-                        }}>
-                            <input type="text" name="email" required onChange={e => setLoginState({
-                                ...loginState,
-                                email: e.target.value,
-                            })} title="email" placeholder="email" />
-                            <br />
-                            <input type="password" name="password" required onChange={e => setLoginState({
-                                ...loginState,
-                                password: e.target.value,
-                            })} title="password" placeholder="password" />
-                            <br />
-                            <button type="submit">Login</button>
-                        </form>
-                    </div>
+                    <Page
+                        className={classes.root}
+                        title="Login"
+                    >
+                        <Box
+                            display="flex"
+                            flexDirection="column"
+                            height="100%"
+                            justifyContent="center"
+                        >
+                            <Container maxWidth="sm">
+                                <Formik
+                                    initialValues={{
+                                        email: 'test@test.com',
+                                        password: 'test123'
+                                    }}
+                                    validationSchema={Yup.object().shape({
+                                        email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+                                        password: Yup.string().max(255).required('Password is required')
+                                    })}
+                                    onSubmit={model => {
+                                        onLogin(model);
+                                    }}
+                                >
+                                    {({
+                                        errors,
+                                        handleBlur,
+                                        handleChange,
+                                        handleSubmit,
+                                        isSubmitting,
+                                        touched,
+                                        values
+                                    }) => (
+                                            <form onSubmit={handleSubmit}>
+                                                <TextField
+                                                    error={Boolean(touched.email && errors.email)}
+                                                    fullWidth
+                                                    helperText={touched.email && errors.email}
+                                                    label="Email Address"
+                                                    margin="normal"
+                                                    name="email"
+                                                    onBlur={handleBlur}
+                                                    onChange={handleChange}
+                                                    type="email"
+                                                    value={values.email}
+                                                    variant="outlined"
+                                                />
+                                                <TextField
+                                                    error={Boolean(touched.password && errors.password)}
+                                                    fullWidth
+                                                    helperText={touched.password && errors.password}
+                                                    label="Password"
+                                                    margin="normal"
+                                                    name="password"
+                                                    onBlur={handleBlur}
+                                                    onChange={handleChange}
+                                                    type="password"
+                                                    value={values.password}
+                                                    variant="outlined"
+                                                />
+                                                <Box my={2}>
+                                                    <Button
+                                                        color="primary"
+                                                        disabled={isSubmitting}
+                                                        fullWidth
+                                                        size="large"
+                                                        type="submit"
+                                                        variant="contained"
+                                                    >
+                                                        Sign in now
+                            </Button>
+                                                </Box>
+                                            </form>
+                                        )}
+                                </Formik>
+                            </Container>
+                        </Box>
+                    </Page>
                 )
             )}
         </AuthContext.Consumer>
