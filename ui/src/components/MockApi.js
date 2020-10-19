7f96aa9eabb43a4aac1ddd0bfd5c380d5e88ba2a
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-10-19 12:19:55 +0300

    Mock Api class

---
+++ ui/src/components/MockApi.js
@@ -0,0 +1,55 @@
+class MockApi {
+    constructor(defaultUsers = [], latency = 500) {
+        this.defaultUsers = defaultUsers;
+        this.latency = latency;
+        this.setUsers(defaultUsers);
+    }
+
+    getAllUsers() {
+        return JSON.parse(localStorage.getItem('mockApiUsers'));
+    }
+
+    setUsers(users) {
+        return localStorage.setItem('mockApiUsers', JSON.stringify(users));
+    }
+
+    signInUser(username, password) {
+        let apiUsers = [];
+        apiUsers = this.getAllUsers();
+        apiUsers.push({ username, password });
+        this.setUsers(apiUsers);
+    }
+
+    authenticate(username, password) {
+        let users = Array(this.getAllUsers());
+        if (users.length === 0) {
+            throw new Error('Empty User Store');
+        }
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                users = Array(this.getAllUsers());
+                let user = users.find((user) => user.username === username && user.password === password);
+                if (user) {
+                    resolve({ error: false, data: users });
+                } else {
+                    reject({ error: true, data: 'Invalid User Credentials' });
+                }
+            }, this.latency);
+        });
+    }
+
+    checkAuthenticate() {
+        const email = localStorage.getItem('email');
+        const password = localStorage.getItem('password');
+        const users = Array(this.getAllUsers());
+        let user = users.find((user) => user.username === email && user.password === password);
+
+        if (user) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+export default MockApi;
