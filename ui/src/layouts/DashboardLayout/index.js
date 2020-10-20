30f2f59473863e49e6028c51230c9a348c0380d5
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-10-20 22:40:08 +0300

    Dashboard components

---
+++ ui/src/layouts/DashboardLayout/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import NavBar from './NavBar';
+import TopBar from './TopBar';
+import propsTypes from 'prop-types';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    display: 'flex',
+    height: '100%',
+    overflow: 'hidden',
+    width: '100%'
+  },
+  wrapper: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 64,
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: 256
+    }
+  },
+  contentContainer: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden'
+  },
+  content: {
+    flex: '1 1 auto',
+    height: '100%',
+    overflow: 'auto'
+  }
+}));
+
+const DashboardLayout = props => {
+  const classes = useStyles();
+  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+
+  return (
+    <div className={classes.root}>
+      <TopBar onMobileNavOpen={() => setMobileNavOpen(true)} />
+      <NavBar
+        onMobileClose={() => setMobileNavOpen(false)}
+        openMobile={isMobileNavOpen}
+      />
+      <div className={classes.wrapper}>
+        <div className={classes.contentContainer}>
+          <div className={classes.content}>
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
+
+DashboardLayout.propsType = {
+  children: propsTypes.node.isRequired,
+};
