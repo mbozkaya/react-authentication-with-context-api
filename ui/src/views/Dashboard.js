b487df3f7638a22506cc22356d00d88b38ba92e0
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-10-20 22:40:21 +0300

    Dashboard & homepage

--- ui/src/views/Dashboard.js
+++ ui/src/views/Dashboard.js
@@ -1,8 +1,49 @@
 import React from 'react';
+import {
+    Container,
+    Grid,
+    makeStyles
+} from '@material-ui/core';
+import Page from '../components/Page';
+import DashboardLayout from '../layouts/DashboardLayout/index';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: theme.palette.background.dark,
+        minHeight: '100%',
+        paddingBottom: theme.spacing(3),
+        paddingTop: theme.spacing(3)
+    }
+}));
 
 const Dashboard = () => {
+
+    const classes = useStyles();
+
     return (
-        <p>Welcome to Dashboard</p>
+        <DashboardLayout>
+            <Page
+                className={classes.root}
+                title="Dashboard"
+            >
+                <Container maxWidth={false}>
+                    <Grid
+                        container
+                        spacing={3}
+                    >
+                        <Grid
+                            item
+                            lg={3}
+                            sm={6}
+                            xl={3}
+                            xs={12}
+                        >
+                            <p>Welcome to Dashboard</p>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Page>
+        </DashboardLayout>
     );
 }
 
--- ui/src/views/Homepage.js
+++ ui/src/views/Homepage.js
@@ -2,16 +2,24 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
 
 function HomePage() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
-                <p>Welcome to Home HomePage</p>
-                <Link className="App-link" to="/login">Login</Link>
-            </header>
-        </div>
+        <AuthContext.Consumer>
+            {
+                ({ authorize }) => (
+                    <div className="App">
+                        <header className="App-header">
+                            <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
+                            <p>Welcome to Home HomePage</p>
+                            {authorize ? <Link className="App-link" to="/dashboard">Dashboard</Link> : <Link className="App-link" to="/login">Login</Link>}
+                        </header>
+                    </div>
+                )
+            }
+        </AuthContext.Consumer>
+
     );
 }
 
