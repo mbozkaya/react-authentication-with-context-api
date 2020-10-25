d1a1ff510dbcc2eead594a8389c5526a916d2c2b
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-10-25 14:52:00 +0300

    sign in button was added

--- ui/src/views/Homepage.js
+++ ui/src/views/Homepage.js
@@ -3,8 +3,17 @@ import logo from '../logo.svg';
 import '../App.css';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles =  makeStyles((theme) => ({
+    wrapper: {
+        paddingLeft:theme.spacing(3),
+    }
+  }));
 
 function HomePage() {
+    const styles = useStyles();
+   
     return (
         <AuthContext.Consumer>
             {
@@ -13,7 +22,12 @@ function HomePage() {
                         <header className="App-header">
                             <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
                             <p>Welcome to Home HomePage</p>
-                            {authorize ? <Link className="App-link" to="/dashboard">Dashboard</Link> : <Link className="App-link" to="/login">Login</Link>}
+                            {authorize ?
+                                <Link className="App-link" to="/dashboard">Dashboard</Link>
+                                : (<div>
+                                    <Link className="App-link"  to="/login">Login</Link>
+                                    <Link className={`App-link ${styles.wrapper}`} to="/signin">Signin</Link>
+                                </div>)}
                         </header>
                     </div>
                 )
