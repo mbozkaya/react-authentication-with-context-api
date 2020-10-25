63075fadc1c8e3a88fde172f325152e8b5a4743e
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-10-25 14:50:25 +0300

    sigin methods and get local storage methods was update

--- ui/src/components/MockApi.js
+++ ui/src/components/MockApi.js
@@ -6,33 +6,50 @@ class MockApi {
     }
 
     getAllUsers() {
-        return JSON.parse(localStorage.getItem('mockApiUsers'));
+        let users = JSON.parse(localStorage.getItem('mockApiUsers'));
+        if (!Array.isArray(users)) {
+            users = Array(users);
+        }
+        return users;
     }
 
     setUsers(users) {
+        if (!Array.isArray(users)) {
+            users = Array(users);
+        }
         return localStorage.setItem('mockApiUsers', JSON.stringify(users));
     }
 
     signInUser(username, password) {
-        let apiUsers = [];
-        apiUsers = this.getAllUsers();
-        apiUsers.push({ username, password });
-        this.setUsers(apiUsers);
+        return new Promise(resolve => {
+            setTimeout(() => {
+                let apiUsers = [];
+                apiUsers = this.getAllUsers();
+                let user = apiUsers.find((user) => user.username === username);
+                if (user) {
+                    resolve({ error: true, message: 'There is a user who have same credentials' });
+                } else {
+                    apiUsers.push({ username, password });
+                    this.setUsers(apiUsers);
+                    resolve({ error: false });
+                }
+            }, this.latency);
+        });
     }
 
     authenticate(username, password) {
-        let users = Array(this.getAllUsers());
+        let users = this.getAllUsers();
         if (users.length === 0) {
             throw new Error('Empty User Store');
         }
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                users = Array(this.getAllUsers());
+                users = this.getAllUsers();
                 let user = users.find((user) => user.username === username && user.password === password);
                 if (user) {
                     resolve({ error: false, data: users });
                 } else {
-                    reject({ error: true, data: 'Invalid User Credentials' });
+                    resolve({ error: true, data: 'Invalid User Credentials' });
                 }
             }, this.latency);
         });
@@ -41,9 +58,9 @@ class MockApi {
     checkAuthenticate() {
         const email = localStorage.getItem('email');
         const password = localStorage.getItem('password');
-        const users = Array(this.getAllUsers());
+        const users = this.getAllUsers();
         let user = users.find((user) => user.username === email && user.password === password);
-        
+
         if (user) {
             return true;
         }
