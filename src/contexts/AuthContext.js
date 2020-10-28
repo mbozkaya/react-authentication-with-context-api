ca04d15b41313b387f40635bbfe3cc44966dcf98
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-10-28 12:59:58 +0300

    layout effect warning disabled

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -89,6 +89,7 @@ const AuthProvider = props => {
                 checkAuth: true,
             });
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
